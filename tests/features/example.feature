286d92097d44d0dea4de3623def0876efa652b43
Sandra Pavan <sandrapavan38@gmail.com>
2024-05-15 21:28:46 -0300

    Inclusão de novos cenarios

--- tests/features/example.feature
+++ tests/features/example.feature
@@ -1,11 +1,49 @@
-Feature: Playwright website
+#language: pt
 
-Scenario: Has Title
-    Given I am at the playwright website
-    # When I open the page
-    Then the title has the text "Playwright"
+Funcionalidade: Realizar ações no site Luma
 
-Scenario: Get Started Link
-    Given I am at the playwright website
-    When I click at "Get Started"
-    Then the URL has the text "intro"
+Contexto: Acessando a tela incial do site
+Dado que eu acesso a tela inicial do Magento
+
+Cenário: Deve acessar a home realizar a busca por look
+E digito o texto "look" na barra de pesquisa
+Quando clico no botão pesquisar
+Então a busca deve ser realizada
+
+Cenário: Deve acessar os itens do Sale, adicionando um item no carrinho
+Quando acesso o menu Sales
+E acesso a opção Bras & Tanks
+E seleciono a opção Antonia Racer Tank
+E seleciono o tamanho XS
+E seleciono a cor Black
+Então clico no botão Add to Cart
+
+Cenário: Deve acessar as politicas de privacidade
+Quando clico no link Privacy and Cookie Policy
+Então a página Políticas de privacidade deve ser aberta
+
+Cenário: Deve acessar um item de compra na home
+Quando seleciono uma opção Argus All-Weather Tank
+E seleciono o tamanho XS
+E seleciono a cor Gray
+Então clico no botão Add to Cart
+
+Cenário: Deve acessar o site e verificar o título da página
+Então verifico se o título da página é Home Page
+
+Cenário: Preencher o login incorretamente
+Quando clico no botão Sign In
+E preencho o e-mail incorretamente
+E preencho a senha incorretamente
+E clico em Sign in
+Então a mensagem de login incorreto é exibido
+
+Cenário: Deve adicionar um produto aos favoritos
+Quando clico em um produto na tela inicial
+Então ao abrir o produto, clico para adicioná-lo aos favoritos
+
+Cenário: Comparar produtos
+Quando clico no link de um produto da tela inicial
+E ao abrir o produto, clico no botão para comparar produtos
+E clico em um produto similar na tela
+Então ao abrir o produto, clico novamente no botão comparar produtos
--- tests/step-definition/example.step.ts
+++ tests/step-definition/example.step.ts
@@ -2,18 +2,112 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 import { ICustomWorld } from '../support/custom-world';
 
-Given('I am at the playwright website', async function (this: ICustomWorld) {
-    await this.page!.goto('https://playwright.dev/');
-});
-  
-When('I click at "Get Started"', async function (this: ICustomWorld) {
-    await this.page!.getByRole('link', { name: 'Get started' }).click();
-});
-  
-Then('the title has the text "Playwright"', async function (this: ICustomWorld) {
-    await expect(this.page!).toHaveTitle(/Playwrights/);//this line is intentionally failing so we can see the reports
+Given('que eu acesso a tela inicial do Magento', async function (this: ICustomWorld) {
+    await this.page!.goto("https://magento.softwaretestingboard.com/");
 });
 
-Then('the URL has the text "intro"', async function (this: ICustomWorld) {
-    await expect(this.page!).toHaveURL(/.*intro/);
+Given('digito o texto {string} na barra de pesquisa', async function (this: ICustomWorld, look: string) {
+    await this.page!.locator('#search').fill(look);
+});
+
+When('clico no botão pesquisar', async function (this: ICustomWorld) {
+    await this.page!.getByRole('button', { name: 'Search' }).click();
+});
+
+Then('a busca deve ser realizada', async function (this: ICustomWorld) {
+    await expect(this.page!.title()).resolves.toMatch("Search results for: 'look'");
+});
+
+When('acesso o menu Sales', async function (this: ICustomWorld) {
+    await this.page!.getByRole('menuitem', { name: 'Sale' }).click();
+});
+
+When('acesso a opção Bras & Tanks', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Bras & Tanks' }).click();
+    await expect(this.page!.title()).resolves.toMatch("Bras & Tanks");
+});
+
+When('seleciono a opção Antonia Racer Tank', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Antonia Racer Tank' }).first().click()
+    await expect(this.page!.title()).resolves.toMatch("Antonia Racer Tank");
+});
+
+When('seleciono o tamanho XS', async function (this: ICustomWorld) {
+    await this.page!.getByLabel('XS').click();
+});
+
+When('seleciono a cor Black', async function (this: ICustomWorld) {
+    await this.page!.getByLabel('Black').click();
+});
+
+Then('clico no botão Add to Cart', async function (this: ICustomWorld) {
+    await this.page!.getByRole('button', { name: 'Add to Cart' }).click();
+});
+
+When('clico no link Privacy and Cookie Policy', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Privacy and Cookie Policy' }).click();
+
+});
+
+Then('a página Políticas de privacidade deve ser aberta', async function (this: ICustomWorld) {
+    await expect(this.page!.title()).resolves.toMatch("Privacy Policy");
+});
+
+When('seleciono uma opção Argus All-Weather Tank', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Argus All-Weather Tank' }).first().click();
+    await expect(this.page!.title()).resolves.toMatch("Argus All-Weather Tank");
+});
+
+When('seleciono a cor Gray', async function (this: ICustomWorld) {
+    await this.page!.getByLabel('Gray').click();
+});
+
+Then('verifico se o título da página é Home Page', async function (this: ICustomWorld) {
+    await expect(await this.page!.title()).toBe('Home Page');
+});
+
+When('clico no botão Sign In', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Sign In' }).click();
+});
+
+When('preencho o e-mail incorretamente', async function (this: ICustomWorld) {
+    await this.page!.getByLabel('Email', { exact: true }).click();
+    await this.page!.getByLabel('Email', { exact: true }).fill('teste@teste.com.br');
+    await this.page!.getByLabel('Email', { exact: true }).press('Tab');
+});
+
+When('preencho a senha incorretamente', async function (this: ICustomWorld) {
+    await this.page!.getByLabel('Password').fill('Teste123');
+});
+
+When('clico em Sign in', async function (this: ICustomWorld) {
+    await this.page!.getByRole('button', { name: 'Sign In' }).click();
+});
+
+Then('a mensagem de login incorreto é exibido', async function (this: ICustomWorld) {
+    const result = await this.page!.getByText('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.').textContent();
+});
+
+When('clico em um produto na tela inicial', async function (this: ICustomWorld) {
+    await this.page!.goto('https://magento.softwaretestingboard.com/radiant-tee.html');
+});
+
+Then('ao abrir o produto, clico para adicioná-lo aos favoritos', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: ' Add to Wish List' }).click();
+});
+
+When('clico no link de um produto da tela inicial', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Breathe-Easy Tank' }).first().click();
+});
+
+When('ao abrir o produto, clico no botão para comparar produtos', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: ' Add to Compare' }).click();      
+});
+
+When('clico em um produto similar na tela', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: 'Gabrielle Micro Sleeve Top' }).first().click();
+});
+
+Then('ao abrir o produto, clico novamente no botão comparar produtos', async function (this: ICustomWorld) {
+    await this.page!.getByRole('link', { name: ' Add to Compare' }).click();
 });
